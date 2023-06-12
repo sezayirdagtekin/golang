4263eddb7e97b5576776b378481f4618af7099e7
Sezayir Dagtekin <sezayir.dagtekin@globallogic.com>
2023-06-12 15:36:56 +0200

    go main connected postrsql database with docker

---
+++ RestApi/DbSelectQuery/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "postgres"
+)
+
+func main() {
+
+	connStr := getConnectionString()
+	//log.Println("connection info:", connStr)
+
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	rows, err := db.Query("select  id, name, inventory,price FROM product")
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var p Product
+
+		rows.Scan(&p.id, &p.name, &p.inventory, &p.price)
+
+		fmt.Println("Product:", p.id, p.name, p.price, p.inventory)
+	}
+}
+
+func getConnectionString() string {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	return psqlInfo
+}
+
+type Product struct {
+	id        int
+	name      string
+	inventory int
+	price     float64
+}
--- RestApi/go.mod
+++ RestApi/go.mod
@@ -3,6 +3,5 @@ module RestApi
 go 1.20
 
 require (
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/mattn/go-sqlite3 v1.14.17 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 )
